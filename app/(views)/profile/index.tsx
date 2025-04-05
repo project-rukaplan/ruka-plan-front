c3c6b0006103ff772cb1642af2a3cd5a249d14eb
luu1003 <juanjot06@gmail.com>
2025-04-05 17:04:50 -0500

    color fondo

--- app/(views)/profile/index.tsx
+++ app/(views)/profile/index.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable import/no-unresolved */
 import { Avatar } from "react-native-paper";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import ProjectGroupPreview from "@/components/ProjectGroupPreview";
@@ -8,25 +7,37 @@ import ProjectGroupPreview from "@/components/ProjectGroupPreview";
 export default function ProfileScreen() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView>
-        <div style={styles.user_info}>
-          <Avatar.Icon size={240} icon={"account"} />
-          <div>
-            <p>Nombre de usuario: </p>
-            <p>Correo del usuario: </p>
+      <View style={styles.full_background}>
+        <SafeAreaView style={styles.screen_background}>
+          <div style={styles.user_info}>
+            <Avatar.Icon size={240} icon={"account"} />
+            <div>
+              <p>Nombre de usuario: </p>
+              <p>Correo del usuario: </p>
+            </div>
           </div>
-        </div>
-      </SafeAreaView>
+        </SafeAreaView>
 
-      <SafeAreaView>
-        <h3>Proyectos actuales:</h3>
-        <ProjectGroupPreview orientation="horizontal"/>
-      </SafeAreaView>
+        <SafeAreaView style={styles.projects_section_background}>
+          <h3>Proyectos actuales:</h3>
+          <ProjectGroupPreview orientation="horizontal" />
+        </SafeAreaView>
+      </View>
     </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  full_background: {
+    flex: 1,
+    backgroundColor: "#F7DCB9",
+  },
+  screen_background: {
+    backgroundColor: "#F7DCB9",
+  },
+  projects_section_background: {
+    backgroundColor: "#F7DCB9", 
+  },
   user_info: {
     padding: 30,
     margin: 40,
